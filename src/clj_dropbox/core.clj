83bb153cd21aec9dd3d544ad53c767f7f2f0da03
samrose <samuel.rose@gmail.com>
2020-09-04 16:49:04 -0400

    sharing links

--- src/clj_dropbox/core.clj
+++ src/clj_dropbox/core.clj
@@ -39,13 +39,40 @@
          file-stream (.getInputStream download)]
     (copy-uri-to-file file-stream dlpath)))
 
-(defn list-metadata [app path]
-  (let [file-list (dbxlist app path)]
-    (.getEntries file-list)))
-;        ;(for [metadata (.getEntries file-list)])]
-;    ;[(.getName metadata) (.getPathLower metadata) (.getRev metatadata)]))
+(defn meta-data [app path]
+   (let [file-list (dbxlist app path)] 
+     (for [metadata (.getEntries file-list)] 
+       { :name (.getName metadata) 
+         :lpath (.getPathLower metadata) 
+         :rev (.getRev metadata)
+         :id (.getId metadata) 
+         :path_display (.getPathDisplay metadata) 
+         :client_modified (.getClientModified metadata)
+         :server_modified (.getServerModified metadata)
+         :is_downloadable (.getIsDownloadable metadata)
+         :sharing_metadata (dbxsharedlinks app (.getPathLower metadata))})))
+         
 
-;(println (list-metadata "cdasdata" "/cdas21research"))
+
+(defn dbxsharedlinks [app path]
+  (let [config (dbxconfig app)
+         client (dbxclient config)
+         sharing (.sharing client)
+         sllist (.listSharedLinksBuilder sharing)
+         wpath (.withPath sllist path)
+         wdirect (.withDirectOnly wpath true)
+         start (.start wdirect)]
+         
+    
+    (try (.getLinks start)
+         (catch Exception e (println e))))) 
+           ;(bean (.getFileMetadata sharing path))))))
+
+
+
+;(println (dbxlist "cdasdata" "/cdas21research"))
+(println (meta-data "cdasdata" "/cdas21research"))
+;(dbxsharedlinks "cdasdata" "/cdas21research/pg op ed article - 2012.pdf")
 
 ;(dbxlist "cdasdata" "/cdas21research")
 ;(dbxdownload "cdasdata" "/cdas21research/local food report_full report.pdf" "5abacf910fd80002860c4" "/home/samrose/Desktop/test-dbxdl/blah5.pdf")
